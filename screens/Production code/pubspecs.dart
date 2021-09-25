93b4ec3d7e9e83e613b85b9fe1331017f483fc3c
Kwesi Welbred <infofeedacc@gmail.com>
2021-09-25 17:04:17 +0000

    pubspecs for fonts, login modification

---
+++ screens/Production code/pubspecs.dart
@@ -0,0 +1,8 @@
+            
+      fonts:
+    - family: ABeeZee
+      fonts:
+        - asset: assets/fonts/ABeeZee-Italic.ttf- asset: assets/fonts/ABeeZee-Regular.ttf
+        weight: normalweight: 400
+      
+          
