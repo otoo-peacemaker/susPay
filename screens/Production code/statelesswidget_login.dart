a35bd75c9a5aa5a2e00c7a365a10a4096c16ded7
Kwesi Welbred <infofeedacc@gmail.com>
2021-09-25 14:19:09 +0000

    screens

---
+++ screens/Production code/statelesswidget_login.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'dart:math' as math;
+import 'dart:math' as math;
+import 'dart:math' as math;
+import 'dart:math' as math;
+
+class 1loginWidget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator 1loginWidget - FRAME
+            return Container(
+      width: 375,
+      height: 812,
+      decoration: BoxDecoration(
+          boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: -336,
+        left: -678,
+        child: null
+      ),Positioned(
+        top: 156,
+        left: 66,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Container(
+      width: 48.82524108886719,
+      height: 48.82524108886719,
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+            bottomRight: Radius.circular(12),
+          ),
+      color : Color.fromRGBO(24, 184, 50, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 11.097286224365234,
+        left: 8.450511932373047,
+        child: Container(
+        width: 30.486431121826172,
+        height: 30.486431121826172,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(30.486431121826172, 30.486431121826172)),
+  )
+      )
+      ),Positioned(
+        top: 12.839367866516113,
+        left: 11.063634872436523,
+        child: Container(
+        width: 27.873308181762695,
+        height: 27.873308181762695,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(27.873308181762695, 27.873308181762695)),
+  )
+      )
+      ),Positioned(
+        top: 13.710409164428711,
+        left: 12.805716514587402,
+        child: Container(
+        width: 26.131227493286133,
+        height: 26.131227493286133,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(26.131227493286133, 26.131227493286133)),
+  )
+      )
+      ),Positioned(
+        top: 22.556598663330078,
+        left: 11.063634872436523,
+        child: Transform.rotate(
+        angle: 22.09488873784485 * (math.pi / 180),
+        child: Container(
+        width: 23.518104553222656,
+        height: 23.518104553222656,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(23.518104553222656, 23.518104553222656)),
+  )
+      ),
+      )
+      ),Positioned(
+        top: 22.556598663330078,
+        left: 11.063634872436523,
+        child: Transform.rotate(
+        angle: 22.09488873784485 * (math.pi / 180),
+        child: Container(
+        width: 23.518104553222656,
+        height: 23.518104553222656,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(23.518104553222656, 23.518104553222656)),
+  )
+      ),
+      )
+      ),Positioned(
+        top: 24.548233032226562,
+        left: 11.934676170349121,
+        child: Transform.rotate(
+        angle: 22.09488873784485 * (math.pi / 180),
+        child: Container(
+        width: 21.866085052490234,
+        height: 21.866085052490234,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(21.866085052490234, 21.866085052490234)),
+  )
+      ),
+      )
+      ),Positioned(
+        top: 5,
+        left: 11.2921142578125,
+        child: Transform.rotate(
+        angle: -12.771392053630018 * (math.pi / 180),
+        child: Container(
+        width: 32.986572265625,
+        height: 32.986572265625,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(32.986572265625, 32.986572265625)),
+  )
+      ),
+      )
+      ),Positioned(
+        top: 21.03833770751953,
+        left: 7.579471588134766,
+        child: Transform.rotate(
+        angle: 22.09488873784485 * (math.pi / 180),
+        child: Container(
+        width: 26.428945541381836,
+        height: 26.428945541381836,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(26.428945541381836, 26.428945541381836)),
+  )
+      ),
+      )
+      ),
+        ]
+      )
+    ), SizedBox(height : 12),
+Text('susPay', textAlign: TextAlign.left, style: TextStyle(
+        color: undefined,
+        fontFamily: 'ABeeZee',
+        fontSize: 48.16416549682617,
+        letterSpacing: -1.5582523345947266,
+        fontWeight: FontWeight.normal,
+        height: 1.2499999405985152
+      ),), SizedBox(height : 12),
+Text('Start managing your Life Finance', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(112, 126, 148, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 16,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),),
+],
+      ),
+    )
+      ),Positioned(
+        top: -0.408203125,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    );
+          }
+        }
+        
