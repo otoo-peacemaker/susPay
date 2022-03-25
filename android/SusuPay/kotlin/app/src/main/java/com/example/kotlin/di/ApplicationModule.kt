89b6899da9df0d75ae89814f43684bb0bc411d19
Peacemaker <peacemaker.otoo@amalitech.org>
2022-03-25 09:21:49 +0000

    resources class added

--- android/SusuPay/kotlin/app/src/main/java/com/example/kotlin/di/ApplicationModule.kt
+++ android/SusuPay/kotlin/app/src/main/java/com/example/kotlin/di/ApplicationModule.kt
@@ -1,2 +1,7 @@
 package com.example.kotlin.di
 
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
--- android/SusuPay/kotlin/app/src/main/java/com/example/kotlin/di/BackendApiModule.kt
+++ android/SusuPay/kotlin/app/src/main/java/com/example/kotlin/di/BackendApiModule.kt
@@ -1,30 +1,43 @@
-package com.example.kotlin.webservices
+package com.example.kotlin.di
 
 import com.example.kotlin.repository.NetworkConnection
+import com.example.kotlin.webservices.Endpoints
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
-class ApiClient {
+@Module
+@InstallIn(SingletonComponent::class)
+object BackendApiModule {
     //interceptor
-    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+    @Singleton
+    @Provides
+    private fun interceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
         this.level = HttpLoggingInterceptor.Level.BODY
     }
 
     //okClient
+    @Singleton
+    @Provides
     private fun client(): OkHttpClient = OkHttpClient.Builder().apply {
-       // this.addInterceptor(TokenInterceptor())
+        // this.addInterceptor(TokenInterceptor())
         this.addInterceptor(NetworkConnection())
-        this.addInterceptor(interceptor)
+        this.addInterceptor(interceptor())
         this.connectTimeout(1, TimeUnit.MINUTES)
         this.writeTimeout(1, TimeUnit.MINUTES)
         this.readTimeout(1, TimeUnit.MINUTES)
     }.build()
 
     //retrofit
-
+    @Singleton
+    @Provides
     fun getRetrofitInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(Endpoints.devBaseURL)
