0adf0b04cd52d7b605ceacf75637eeee905b6d8b
Peacemaker <peacemaker.otoo@amalitech.org>
2022-04-19 20:14:35 +0000

    layout xmls added

--- android/SusuPay/kotlin/app/src/main/java/com/example/kotlin/util/Extension.kt
+++ android/SusuPay/kotlin/app/src/main/java/com/example/kotlin/util/Extension.kt
@@ -6,7 +6,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.kotlin.network.Resource
 import com.example.kotlin.ui.fragment.login.LoginFragment
-import com.example.kotlin.ui.fragment.RegistrationFragment
+import com.example.kotlin.ui.fragment.register.RegistrationFragment
 import com.google.android.material.snackbar.Snackbar
 
 
